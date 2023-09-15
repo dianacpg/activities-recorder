3a20ea4c739cf3ca1a9ec0e6376d4491d442662b
dianacpg <dianacpgouveia@gmail.com>
2023-09-15 14:57:44 +0100

    feat: add dialog

---
+++ src/components/dialog/test/indext.unit.test.tsx
@@ -0,0 +1,39 @@
+import { expect, describe, it, vi } from "vitest";
+import { render, fireEvent } from "@testing-library/react";
+import Dialog from "..";
+
+describe("Dialog", () => {
+  const defaultProps = {
+    title: "Sample Dialog",
+    description: "This is a test dialog.",
+    onConfirm: vi.fn(),
+    onCancel: vi.fn(),
+  };
+
+  it("renders the dialog with the provided title and description", () => {
+    const { getByText } = render(<Dialog {...defaultProps} />);
+
+    // Ensure that the title and description are rendered
+    expect(getByText("Sample Dialog")).toBeInTheDocument();
+    expect(getByText("This is a test dialog.")).toBeInTheDocument();
+  });
+
+  it("calls onCancel when the 'Cancel' button is clicked", () => {
+    const { getByTestId } = render(<Dialog {...defaultProps} />);
+    const cancelButton = getByTestId("cancel-dialog");
+    fireEvent.click(cancelButton);
+
+    expect(defaultProps.onCancel).toHaveBeenCalledTimes(1);
+  });
+
+  it("calls onConfirm when the 'Confirm' button is clicked", () => {
+    const { getByTestId } = render(<Dialog {...defaultProps} />);
+    const confirmButton = getByTestId("confirm-dialog");
+
+    // Simulate a click on the 'Confirm' button
+    fireEvent.click(confirmButton);
+
+    // Verify that onConfirm has been called
+    expect(defaultProps.onConfirm).toHaveBeenCalled();
+  });
+});
