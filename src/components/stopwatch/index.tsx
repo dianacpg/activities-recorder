e4a381449837c7e26f1a17e41aefdf91a7b3a774
dianacpg <dianacpgouveia@gmail.com>
2023-09-15 15:19:20 +0100

    feat: add stopwatch

---
+++ src/components/stopwatch/index.tsx
@@ -0,0 +1,75 @@
+// React
+import { useEffect, useState, ReactElement, useMemo } from "react";
+// Style
+import styles from "./styles/stopwatch.module.scss";
+// Utils
+import { formatSeconds } from "../../lib/utils/format-seconds";
+
+interface StopwatchProps {
+  /** function to call when stop Stopwatch */
+  onStop: (dateStart: string | undefined) => void;
+}
+
+/**
+ * Component that provides recording functionality with a timer.
+ */
+
+const Stopwatch = ({ onStop }: StopwatchProps): ReactElement => {
+  // Event Start Date
+  const [dateStart, setDateStart] = useState<string | undefined>(undefined);
+  // Stopwatch time
+  const [seconds, setSeconds] = useState(0);
+  // Counter in hh:mm:ss style
+  const counterText = useMemo(() => formatSeconds(seconds), [seconds]);
+
+  /**
+   * Toggles the recording state.
+   */
+  const handleToggleRecording = () => {
+    if (dateStart) {
+      onStop(dateStart);
+      setDateStart(undefined);
+    } else {
+      setDateStart(new Date().toISOString());
+    }
+  };
+
+  /**
+   *  Update the recording time and manage the interval.
+   */
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout | undefined = undefined;
+
+    if (dateStart) {
+      intervalId = setInterval(() => {
+        const currentTime = Date.now();
+        const startTime = new Date(dateStart).getTime();
+        const elapsedSeconds = Math.floor((currentTime - startTime) / 1000);
+        setSeconds(elapsedSeconds);
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+      setSeconds(0); // Reset seconds when not recording
+    }
+
+    return () => {
+      if (!intervalId) return;
+      clearInterval(intervalId);
+    };
+  }, [dateStart]);
+
+  return (
+    <div className={`${styles.stopwatch} ${dateStart ? styles["stopwatch--started"] : ""}`}>
+      <button
+        onClick={handleToggleRecording}
+        className={styles["stopwatch-button"]}
+        data-testid="stopwatch-button"
+      >
+        <span></span>
+      </button>
+      <div className={styles["stopwatch-counter"]}>{counterText}</div>
+    </div>
+  );
+};
+
+export default Stopwatch;
---
+++ src/components/stopwatch/test/index.unit.test.tsx
@@ -0,0 +1,44 @@
+import "@testing-library/jest-dom/extend-expect";
+import { expect, describe, it, vi } from "vitest";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import Stopwatch from "..";
+
+// Mock the formatSeconds function
+/* jest.mock("../../../lib/utils/format-seconds", () => ({
+  formatSeconds: (seconds: number) => `mocked:${seconds}`,
+})); */
+
+describe("Stopwatch component", () => {
+  const onStopMock = vi.fn();
+  const testComponent = <Stopwatch onStop={onStopMock} />;
+
+  it("renders without errors", () => {
+    render(testComponent);
+    const stopwatchButton = screen.getByRole("button");
+    expect(stopwatchButton).toBeInTheDocument();
+    expect(stopwatchButton).toHaveClass("stopwatch-button");
+  });
+
+  it("starts recording when the button is clicked and stops when clicked again", async () => {
+    render(testComponent);
+
+    const toggleButton = screen.getByRole("button");
+
+    fireEvent.click(toggleButton); // Start recording
+    await waitFor(() => expect(onStopMock).not.toHaveBeenCalled());
+
+    fireEvent.click(toggleButton); // Stop recording
+    await waitFor(() => expect(onStopMock).toHaveBeenCalled());
+  });
+
+  /*  it("displays the elapsed time while recording", async () => {
+    render(testComponent);
+
+    const toggleButton = screen.getByRole("button");
+
+    fireEvent.click(toggleButton); // Start recording
+    await waitFor(() => expect(screen.getByText("mocked:1")).toBeInTheDocument());
+
+    fireEvent.click(toggleButton); // Stop recording
+  }); */
+});
