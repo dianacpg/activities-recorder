a4f2b51dd24a5be28cf4e0829cc33009846f4e47
dianacpg <dianacpgouveia@gmail.com>
2023-09-15 15:20:13 +0100

    refactor: add utils for calendar

---
+++ src/lib/utils/create-date-key.ts
@@ -0,0 +1,30 @@
+import { addZero } from "./add-zero";
+
+/**
+ * Creates a date key in the format 'YYYY-MM-DD' based on the provided date.
+ *
+ * @param {Date} date - The date for which the key is to be created.
+ * @returns {string} The date key in the format 'YYYY-MM-DD'.
+ */
+
+interface CreateDateKey {
+  year: number;
+  month: number;
+  day: number;
+  hour: number;
+  minute: number;
+  fullTime: string;
+  fullDate: string;
+}
+
+export const createDateKey = (date: Date): CreateDateKey => {
+  const year = date.getUTCFullYear();
+  const month = date.getUTCMonth() + 1;
+  const day = date.getUTCDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const fullDate = `${year}-${addZero(month)}-${addZero(day)}`;
+  const fullTime = `${addZero(hour)}:${addZero(minute)}`;
+
+  return { year, month, day, hour, minute, fullDate, fullTime };
+};
---
+++ src/lib/utils/group-events-by-day.ts
@@ -0,0 +1,43 @@
+import { GroupedEvents } from "../../store/selectors/user-events";
+import { UserEvent } from "../services";
+import { createDateKey } from "./create-date-key";
+
+/**
+ * Adds an event to the group corresponding to the given date key.
+ *
+ * @param {string} dateKey - The date key for grouping events (in the format 'YYYY-MM-DD').
+ * @param {UserEvent} event - The UserEvent object to be added to the group.
+ * @param {GroupedEvents} groups - groups where event is being injected
+ */
+
+// Adds an event to the group corresponding to the given date key.
+const addToGroup = (dateKey: string, event: UserEvent, groups: GroupedEvents) => {
+  if (groups[dateKey] === undefined) {
+    groups[dateKey] = [];
+  }
+
+  groups[dateKey].push(event);
+};
+
+/**
+ * Groups an array of UserEvent objects by day based on their dateStart and dateEnd properties.
+ *
+ * @param {UserEvent[]} events - An array of UserEvent objects to be grouped.
+ * @returns {GroupedEvents} An object with date keys as properties and arrays of UserEvent objects as values.
+ */
+
+export const groupEventsByDay = (events: UserEvent[]): Record<string, UserEvent[]> => {
+  const groups: GroupedEvents = {};
+
+  events.forEach((event) => {
+    const dateStartKey = createDateKey(new Date(event.dateStart)).fullDate;
+    const dateEndKey = createDateKey(new Date(event.dateEnd)).fullDate;
+    addToGroup(dateStartKey, event, groups);
+
+    if (dateEndKey !== dateStartKey) {
+      addToGroup(dateEndKey, event, groups);
+    }
+  });
+
+  return groups;
+};
