b070635d9bd4997d5e60ccd308d361f5c22c870f
dianacpg <dianacpgouveia@gmail.com>
2023-09-15 14:55:16 +0100

    feat: add calendar

---
+++ src/components/calendar/index.tsx
@@ -0,0 +1,41 @@
+// React
+import { ReactElement } from "react";
+// Components
+import DayItems from "../day-items";
+import EmptyState from "../empty-state";
+// Store
+import { GroupedEventsData } from "../../store/selectors/user-events";
+import { UserEvent } from "../../lib/services";
+
+interface CalendarProps {
+  events: GroupedEventsData | undefined;
+  onDelete: (id: number) => void;
+  onUpdate: (title: string, event: UserEvent) => void;
+}
+
+const Calendar = ({ events, onDelete, onUpdate }: CalendarProps): ReactElement => {
+  if (!events?.groupedEvents) return <EmptyState />;
+  return (
+    <>
+      {events?.sortedGroupKeys?.map((dayKey, i) => {
+        const dayEvents = events?.groupedEvents ? events?.groupedEvents[dayKey] : [];
+        const groupDate = new Date(dayKey);
+        const day = groupDate.getDate();
+        const month = groupDate.toLocaleDateString(undefined, { month: "short" });
+
+        return (
+          <DayItems
+            key={`day-item-${dayKey}-${i}`}
+            day={day}
+            month={month}
+            events={dayEvents}
+            onDelete={onDelete}
+            onUpdate={onUpdate}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+export default Calendar;
---
+++ src/components/calendar/test/index.unit.test.tsx
@@ -0,0 +1,37 @@
+import { expect, describe, it, vi } from "vitest";
+// React testing library
+import { render } from "@testing-library/react";
+import Calendar from "..";
+import { GroupedEventsData } from "../../../store/selectors/user-events";
+
+describe("Calendar", () => {
+  const mockEvents: GroupedEventsData = {
+    sortedGroupKeys: ["2023-08-24"],
+    groupedEvents: {
+      "2023-08-24": [
+        { id: 1, title: "Event 1", dateStart: "monday", dateEnd: "friday" },
+        { id: 2, title: "Event 2", dateStart: "monday", dateEnd: "friday" },
+      ],
+    },
+  };
+
+  it("renders empty state if no events", () => {
+    const { container } = render(
+      <Calendar
+        events={{ sortedGroupKeys: undefined, groupedEvents: undefined }}
+        onDelete={() => vi.fn()}
+        onUpdate={() => vi.fn()}
+      />
+    );
+
+    expect(container.querySelector(".empty-state")).toBeInTheDocument();
+  });
+
+  it("renders day items if has events", () => {
+    const { getAllByText } = render(
+      <Calendar events={mockEvents} onDelete={() => vi.fn()} onUpdate={() => vi.fn()} />
+    );
+    const eventTitles = getAllByText(/Event/);
+    expect(eventTitles).toHaveLength(2);
+  });
+});
